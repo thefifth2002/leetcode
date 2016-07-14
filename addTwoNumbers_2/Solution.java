df53ba1640c892f64e4af2a110cc0b3020a10d7d
hao wu <hw1395@nyu.edu>
2016-07-14 00:32:03 -0400

    add two numbers
    finished

--- addTwoNumbers_2/Solution.java
+++ addTwoNumbers_2/Solution.java
@@ -1,7 +1,7 @@
 package addTwoNumbers_2;
 
 public class Solution {
-  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+  public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
     ListNode i = new ListNode(0);
     ListNode result = i;
     int carrier = 0;
@@ -23,6 +23,30 @@ public class Solution {
     }
     if (carrier > 0) {
       i.next = new ListNode(carrier);
+    }
+    return result;
+  }
+  
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode i = new ListNode(0);
+    ListNode result = i;
+    while (l1 != null || l2 != null) {
+      if (l1 != null) {
+        i.val += l1.val;
+        l1 = l1.next;
+      }
+      if (l2 != null) {
+        i.val += l2.val;
+        l2 = l2.next;
+      }
+      if (l1 != null || l2 != null) {
+        i.next = new ListNode(i.val / 10);
+        i.val %= 10;
+        i = i.next;
+      }
+    }
+    if (i.val > 10) {
+      i.next = new ListNode(i.val);
     }
     return result;
   }
