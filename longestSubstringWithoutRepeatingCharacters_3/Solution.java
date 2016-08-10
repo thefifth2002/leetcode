0bd7297044639edf33cc8776aa902f7fbf234b91
hao wu <hw1395@nyu.edu>
2016-08-10 18:05:37 -0400

    q3
    q3

--- longestSubstringWithoutRepeatingCharacters_3/Solution.java
+++ longestSubstringWithoutRepeatingCharacters_3/Solution.java
@@ -2,9 +2,17 @@ package longestSubstringWithoutRepeatingCharacters_3;
 
 public class Solution {
   public int lengthOfLongestSubstring(String s) {
+    if (s == null) {
+      return 0;
+    }
+    
+    char[] charArray = s.toCharArray();
     int maxLength = 0;
+    String sub = "";
     for (int i = 0; i < s.length(); i++) {
-      
+      for (int j = i; j < s.length(); j++) {
+        sub = s.substring(i,j);
+      }
     }
     return 0;
   }
