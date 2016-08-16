9d957f5b2b5b856db74b0db739a11b7877b44906
thefifth2002 <thefifth2002@hotmail.com>
2016-08-16 03:09:31 -0400

    qustion 3 exceed time limit

--- longestSubstringWithoutRepeatingCharacters_3/Solution.java
+++ longestSubstringWithoutRepeatingCharacters_3/Solution.java
@@ -1,17 +1,26 @@
 package longestSubstringWithoutRepeatingCharacters_3;
 
+import java.util.HashMap;
+
 public class Solution {
+
+
   public int lengthOfLongestSubstring(String s) {
     if (s == null) {
       return 0;
     }
-
     char[] charArray = s.toCharArray();
     int maxLength = 0;
-    String sub = "";
+    HashMap<Character, Integer> map = new HashMap<Character, Integer>();
     for (int i = 0; i < charArray.length; i++) {
-
+      if (!map.containsKey(charArray[i])) {
+        map.put(charArray[i], i);
+      } else {
+        maxLength = Math.max(maxLength, map.size());
+        i = map.get(charArray[i]);
+        map.clear();
+      }
     }
-    return 0;
+    return maxLength = Math.max(maxLength, map.size());
   }
 }
