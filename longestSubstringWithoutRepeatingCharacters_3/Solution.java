bb45d155a05d2cad1881caecccde6a6cdc85fb05
thefifth2002 <thefifth2002@hotmail.com>
2016-08-20 00:34:24 -0400

    p3

--- longestSubstringWithoutRepeatingCharacters_3/Solution.java
+++ longestSubstringWithoutRepeatingCharacters_3/Solution.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Solution {
 
 
-  public int lengthOfLongestSubstring(String s) {
+  public int lengthOfLongestSubstring1(String s) {
     if (s == null) {
       return 0;
     }
@@ -22,5 +22,30 @@ public class Solution {
       }
     }
     return maxLength = Math.max(maxLength, map.size());
+  }
+
+  public int lengthOfLongestSubstring(String s) {
+    if (s == null) {
+      return 0;
+    }
+
+    HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+    int max = 0;
+    int start = 0;
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (!map.containsKey(c)) {
+        map.put(c, i);
+      } else {
+        max = Math.max(max, map.size());
+        while (map.containsKey(c)) {
+          map.remove(s.charAt(start));
+          start++;
+        }
+        map.put(c, i);
+      }
+    }
+    max = Math.max(max, map.size());
+    return max;
   }
 }
