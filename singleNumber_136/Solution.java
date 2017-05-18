88b0f158a41d5580d10521d1f401a1b64ccd5e9a
thefifth2002 <thefifth2002@hotmail.com>
2017-05-18 05:42:11 -0400

    136： Single Number

---
+++ singleNumber_136/Solution.java
@@ -0,0 +1,11 @@
+package singleNumber_136;
+
+public class Solution {
+  public int singleNumber(int[] nums) {
+    int res = 0;
+    for (int i = 0; i < nums.length; i++) {
+      res = res ^ nums[i];
+    }
+    return res;
+  }
+}
---
+++ singleNumber_136/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Hao
+ *
+ */
+package singleNumber_136;
