a049c07f47b9bba5ba18af7fbb6a277dc4ae27f2
thefifth2002 <thefifth2002@hotmail.com>
2017-05-25 13:10:56 -0400

    42： Trapping Rain Water

---
+++ trappingRainWater_42/Solution.java
@@ -0,0 +1,44 @@
+package trappingRainWater_42;
+
+public class Solution {
+  public int trap(int[] height) {
+    int water = 0;
+    int left = 0;
+    int right = height.length - 1;
+    while (left < right && height[left] <= height[left + 1]) {
+      left++;
+    }
+    while (left < right && height[right - 1] >= height[right]) {
+      right--;
+    }
+    while (left < right) {
+      int leftEdge = height[left];
+      int rightEdge = height[right];
+      if (leftEdge <= rightEdge) {
+        while (left < right && leftEdge >= height[left + 1]) {
+          water = water + leftEdge - height[left + 1];
+          System.out.println("left = " + left);
+          System.out.println("water = " + water);
+          left++;
+        }
+        left++;
+      } else {
+        while (left < right && height[right - 1] <= rightEdge) {
+          water = water + rightEdge - height[right - 1];
+          System.out.println("right = " + right);
+          System.out.println("water = " + water);
+          right--;
+        }
+        right--;
+      }
+    }
+    return water;
+  }
+  public static void main(String[] args) {
+    Solution sol = new Solution();
+    int[] h = {2, 0, 2};
+    int[] h1 = {0,1,0,2,1,0,1,3,2,1,2,1};
+//    System.out.println(sol.trap(h));
+    System.out.println(sol.trap(h1));
+  }
+}
