bb2c33c78a49c1b05e0c7bbc7adeccdf71e7fc5c
thefifth2002 <thefifth2002@hotmail.com>
2017-06-05 13:00:39 -0400

    338: Counting Bits

--- powerOfFour_342/Solution.java
+++ powerOfFour_342/Solution.java
@@ -1,5 +1,18 @@
 package powerOfFour_342;
 
 public class Solution {
-
+  /*/
+   * Since, one wants to find if it is the power of 4.
+   * It means that the number is of the form
+   * 4^n = 2 ^(2n) i.e one of the 2nth bit will be set to 1.
+   * i.e if the number is power of 4, then either bit at
+   * index 0, 2, 4 and so on will be set to 1. Remember,
+   * there will be only one bit set but which one will be set is not known.
+   * Look at the input integer as a set of eight nibble(4 bits)
+   * each hexadecimal can be rewrote as a 4 digital binary. 0x means hexadecimal,
+   * 5 is 4+1 = 2^2 +2^0. So 5 is 0101. 0x555 = 0101 0101 0101.
+   */
+  public boolean isPowerOfFour(int num) {
+    return num > 0 && (num & num - 1) ==0 && (num & 0x55555555) !=0;
+  }
 }
