865078b68b5b18d0b10a23e5ae615cb1f9bb0a1c
thefifth2002 <thefifth2002@hotmail.com>
2017-06-13 22:09:30 -0400

    80: Remove Duplicates from Sorted Array

---
+++ removeDuplicatesFromSortedArrayII_80/Solution.java
@@ -0,0 +1,14 @@
+package removeDuplicatesFromSortedArrayII_80;
+
+public class Solution {
+  public int removeDuplicates(int[] nums) {
+    int i = 0;
+    for (int n : nums) {
+      if (i < 2 || n > nums[i - 2]) {
+        nums[i] = n;
+        i++;
+      }
+    }
+    return i;
+  }
+}
---
+++ removeDuplicatesFromSortedArrayII_80/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Hao
+ *
+ */
+package removeDuplicatesFromSortedArrayII_80;
