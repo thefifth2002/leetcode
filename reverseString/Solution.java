e7b292bf1e822855f85d5f6ddf1b1ad6e39ab421
thefifth2002 <thefifth2002@hotmail.com>
2017-06-28 19:50:19 -0400

    344. Reverse String

---
+++ reverseString/Solution.java
@@ -0,0 +1,18 @@
+package reverseString;
+
+public class Solution {
+  public String reverseString(String s) {
+    char[] chars = s.toCharArray();
+    int ln = chars.length;
+    for (int i = 0; i < ln / 2; i++) {
+      swap(chars, i, ln - 1 - i);
+    }
+    return new String(chars);
+  }
+
+  private void swap(char[] chars, int i, int j) {
+    char tmp = chars[i];
+    chars[i] = chars[j];
+    chars[j] = tmp;
+  }
+}
