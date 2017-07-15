bdca5fc9cbd1932b1a31c9cc302c7b44c217e3c9
thefifth2002 <thefifth2002@hotmail.com>
2017-07-15 15:08:42 -0400

    437. Path Sum III

---
+++ pathSumIII_437/Solution.java
@@ -0,0 +1,19 @@
+package pathSumIII_437;
+import treeSetUp.TreeNode;
+
+public class Solution {
+  public int pathSum(TreeNode root, int sum) {
+    if (root == null) {
+      return 0;
+    }
+    return helper(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+  }
+
+  private int helper(TreeNode root, int sum) {
+    if (root == null) {
+      return 0;
+    }
+    return (root.val == sum ? 1 : 0) + helper(root.left, sum - root.val)
+        + helper(root.right, sum - root.val);
+  }
+}
---
+++ pathSumIII_437/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Hao
+ *
+ */
+package pathSumIII_437;
