998fa6c6fafc125e54b4da517ae20bde222f7645
thefifth2002 <thefifth2002@hotmail.com>
2017-07-18 13:02:24 -0400

    575: Distribute Candies

---
+++ distributeCandies_575/Solution.java
@@ -0,0 +1,13 @@
+package distributeCandies_575;
+
+import java.util.HashSet;
+
+public class Solution {
+  public int distributeCandies(int[] candies) {
+    HashSet<Integer> set = new HashSet<>();
+    for (int candy : candies) {
+      set.add(candy);
+    }
+    return Math.min(set.size(), candies.length / 2);
+  }
+}
---
+++ distributeCandies_575/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Hao
+ *
+ */
+package distributeCandies_575;
