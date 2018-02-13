e6338401c7aaec5af5ee2e2a42f09e77e68d3f44
thefifth2002 <thefifth2002@hotmail.com>
2018-02-13 12:34:27 -0500

    653. Two Sum IV - Input is a BST

---
+++ p653twoSumIVInputIsABST/Solution.java
@@ -0,0 +1,22 @@
+package p653twoSumIVInputIsABST;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+  public boolean findTarget(TreeNode root, int k) {
+    Set<Integer> set = new HashSet<>();
+    return helper(root, set, k);
+  }
+
+  private boolean helper(TreeNode root, Set<Integer> set, int k) {
+    if (root == null) {
+      return false;
+    }
+    if (set.contains(k - root.val)) {
+      return true;
+    }
+    set.add(root.val);
+    return helper(root.left, set, k) || helper(root.right, set, k);
+  }
+}
---
+++ p653twoSumIVInputIsABST/TreeNode.java
@@ -0,0 +1,10 @@
+package p653twoSumIVInputIsABST;
+
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode(int x) {
+    val = x;
+  }
+}
