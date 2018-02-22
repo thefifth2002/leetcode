9437b8a42f51d0b0693b6b8983cf39d9b7d7ca98
thefifth2002 <thefifth2002@hotmail.com>
2018-02-22 18:18:40 -0500

    232. Implement Queue using Stacks

---
+++ p232ImplementQueueUsingStacks/MyQueue.java
@@ -0,0 +1,30 @@
+package p232ImplementQueueUsingStacks;
+
+import java.util.Stack;
+
+public class MyQueue {
+  Stack<Integer> in;
+  Stack<Integer> out;
+  public MyQueue() {
+    in = new Stack<>();
+    out = new Stack<>();
+  }
+  public void push(int x) {
+    in.push(x);
+  }
+  public int pop() {
+    peek();
+    return out.pop();
+  }
+  public int peek() {
+    if (out.empty()) {
+      while (!in.empty()) {
+        out.push(in.pop());
+      }
+    }
+    return out.peek();
+  }
+  public boolean empty() {
+    return in.empty() && out.empty();
+  }
+}
---
+++ p232ImplementQueueUsingStacks/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Hao
+ *
+ */
+package p232ImplementQueueUsingStacks;
